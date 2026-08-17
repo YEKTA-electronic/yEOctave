112c87d13c299203dd0927f10c6881b0ab2e84b6
hoseinAli <hoseinelectronic@gmail.com>
2026-08-18 00:43:49 +0330

    fixed bug
    char is not Scalar, but acceptable!

--- @yoSQL/scalar2str.m
+++ @yoSQL/scalar2str.m
@@ -3,6 +3,9 @@ function str = scalar2str(obj,x)
 	if nargin==0 || isempty(x)
 		str = 'NULL';
 
+	elseif ischar(x)
+		str = ['"',x,'"'];% put "" around a sq'' string
+
 	elseif !isscalar(x)
 		error('Scalar to String | Not SCALAR input')
 
@@ -15,9 +18,6 @@ function str = scalar2str(obj,x)
 		elseif isfloat(x)
 			str = num2str(x);
 		endif
-
-	elseif ischar(x)
-		str = ['"',x,'"'];% put "" around a sq'' string
 
 	elseif isstruct(x)
 		str = jsonencode(x);
