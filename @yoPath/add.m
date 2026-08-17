e99d2fa45d72c8fdee0e08a5ece75b5d1108e43a
hoseinAli <hoseinelectronic@gmail.com>
2026-08-17 15:05:02 +0330

    AUTO mode
    just by initiating an instance, all the works are done automatically!

--- @yoPath/add.m
+++ @yoPath/add.m
@@ -2,7 +2,7 @@ function add(obj)
 	% convert back to char/string
 	pathStr = strjoin(obj.dirInc,';');
 	% apply
-	%addpath(pathStr);
+	addpath(pathStr);
 	rehash();
 	disp('All the INCLUSION list paths are added now')
 endfunction
--- @yoPath/yoPath.m
+++ @yoPath/yoPath.m
@@ -8,12 +8,32 @@ classdef yoPath
 	endproperties
 %% METHODs ::::::::::::::::::::::::::::::::::::::::::::::
 	methods
-		function obj = yoPath(rootFolder)
-			if nargin<1
-				rootFolder = pwd;% default
-			endif
-			obj.dirRoot = rootFolder;
+		function obj = yoPath(rootFolder,excList)
+			isAuto = 0;
+			% where the class is placed
 			obj.dirMain = fileparts(__FILE__);
+
+			if nargin==0
+				% default mode
+				rootFolder = pwd;
+			elseif nargin == 1
+				% manual mode
+				obj.dirRoot = rootFolder;
+				obj.dirExc = cell();
+			elseif nargin == 2
+				% auto mode
+				obj.dirRoot = rootFolder;
+				obj.dirExc = excList;
+				isAuto = 1;
+			else
+				error('yoPath | bad init')
+			endif
+
+			if isAuto
+				obj.dirFind = obj.search();
+				obj.dirInc = obj.filter();
+				obj.add();
+			endif
 
 		endfunction
 	endmethods
